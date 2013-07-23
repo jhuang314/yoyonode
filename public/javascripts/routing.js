03ce26fd4779768e12d050d238bcadbe830e7428
Jesse Huang <jh3141@gmail.com>
2013-07-23 00:13:39 -0400

    added yoyo files

--- public/javascripts/routing.js
+++ public/javascripts/routing.js
@@ -2,24 +2,91 @@ angular.module('app', [])
 
 // Module loading
 // --------------
-.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-  'use strict';
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	'use strict';
 
-    var DEFAULT_PATH = "404";
+	var DEFAULT_PATH = "/";
 
-    $routeProvider.when('/home', {
+	$routeProvider
+	
+	    .when('/', {
 
-      templateUrl: '/javascripts/tmpl/home.html',
-      controller : 'homeCtrl',
+		templateUrl: '/javascripts/tmpl/index.html',
+		controller : 'homeCtrl',
 
-    }).when('/404', {
-      templateUrl: '/javascripts/tmpl/404.html',
-      controller : 'missingPageCtrl',
-    })
+	    })
+
+	    .when('/about', {
+		templateUrl: '/javascripts/tmpl/about.html',
+		controller : 'homeCtrl',
+	    })
 
 
-    .otherwise({
-      redirectTo : DEFAULT_PATH,
-    });
+	    .when('/archives', {
 
-}]);
+		templateUrl: '/javascripts/tmpl/archives.html',
+		controller : 'homeCtrl',
+
+	    })
+
+	    .when('/contact', {
+
+		templateUrl: '/javascripts/tmpl/contact.html',
+		controller : 'homeCtrl',
+
+	    })
+
+	    .when('/equiptment', {
+
+		templateUrl: '/javascripts/tmpl/equiptment.html',
+		controller : 'homeCtrl',
+
+	    })
+
+	    .when('/events', {
+
+		templateUrl: '/javascripts/tmpl/events.html',
+		controller : 'homeCtrl',
+
+	    })
+
+	    .when('/members', {
+
+		templateUrl: '/javascripts/tmpl/members.html',
+		controller : 'homeCtrl',
+
+	    })
+
+	    .when('/picture', {
+
+		templateUrl: '/javascripts/tmpl/picture.html',
+		controller : 'homeCtrl',
+
+	    })
+
+	    .when('/tutorial', {
+
+		templateUrl: '/javascripts/tmpl/tutorial.html',
+		controller : 'homeCtrl',
+
+	    })
+
+
+	    .when('/home', {
+
+		templateUrl: '/javascripts/tmpl/home.html',
+		controller : 'homeCtrl',
+
+	    })
+	
+	    .when('/404', {
+		templateUrl: '/javascripts/tmpl/404.html',
+		controller : 'missingPageCtrl',
+	    })
+
+
+	    .otherwise({
+		redirectTo : DEFAULT_PATH,
+	    });
+
+    }]);
