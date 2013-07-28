6f2715df7d97885c07f2418c9edd0723e7f9d130
Jesse Huang <jh3141@gmail.com>
2013-07-28 18:57:20 -0400

    updated controllers

--- public/javascripts/controller/home.js
+++ public/javascripts/controller/home.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('homeCtrl', ['$scope', function ($scope) {
   'use strict';
 
-  $scope.title = "Apply For Your VA Health Benefits or Update Your Information";
+  // $scope.title = "";
 }]);
