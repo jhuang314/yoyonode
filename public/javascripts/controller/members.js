0370ae8dd8e49448efee0183dc074f167b78b848
Jesse Huang <jh3141@gmail.com>
2013-07-28 23:14:30 -0400

    used angularjs repeat feature for members

--- public/javascripts/controller/members.js
+++ public/javascripts/controller/members.js
@@ -1,5 +1,16 @@
-angular.module('app').controller('memberCtrl', ['$scope', function ($scope) {
-  'use strict';
-
-  $scope.title = "";
+angular.module('app').controller('memberCtrl', ['$scope', '$http', function ($scope, $http) {
+    'use strict';
+    
+    $http.get('/data/members.json').success(function(data) {
+    	$scope.members = data;
+	$scope.numColumns = 3;
+	$scope.rows = [];
+	$scope.rows.length = Math.ceil($scope.members.length / $scope.numColumns);
+	$scope.cols = [];
+	$scope.cols.length = $scope.numColumns;    
+    });
+    
+    $scope.orderProp = 'name';
+    
 }]);
+
